37cc071a4c3f77f8a39938390c7a54abac25e496
Rayan Ahmed <rayansteelpak123@gmail.com>
2025-07-22 09:44:29 -0700

    Add files via upload

---
+++ Mark_Sheet/Mark_Sheet/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mark_Sheet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name");
+            string Std_name = Console.ReadLine();
+            Console.WriteLine("Enter Your Age");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Math-Number");
+            int math = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your English-Number");
+            int eng = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Your Urdu-Number");
+            int urdu = Convert.ToInt32(Console.ReadLine());
+            int total = math + eng + urdu;
+            float percent = total / 300F * 100;
+            string grade ;
+            Console.WriteLine("MarkSheet");
+            Console.WriteLine($"Student_Name : {Std_name}");
+            Console.WriteLine($"Student_Age : {age}");
+            Console.WriteLine($"Math_Marks : {math}");
+            Console.WriteLine($"English_Marks : {eng}");
+            Console.WriteLine($"Urdu_Marks : {urdu}");
+            Console.WriteLine($"Total_Marks : {total}");
+            Console.WriteLine($"Percentage : {percent}");
+            if (percent >= 80 && math >=33 && urdu>=33 && eng>=33)
+            {
+                grade = "A1";
+            }
+          /*  Console.WriteLine($"Grade :{grade}")*/;
+        }
+    }
+}
