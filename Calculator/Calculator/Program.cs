3492d84c3e241b999c63bc8501de12aaa2387bea
Rayan Ahmed <rayansteelpak123@gmail.com>
2025-07-22 09:38:14 -0700

    Add files via upload

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Basic Calculator");
+            Console.Write("Enter first number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter an operator (+, -, *, /): ");
+            char op = Convert.ToChar(Console.ReadLine());
+
+            Console.Write("Enter second number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double result;
+
+            switch (op)
+            {
+                case '+':
+                    result = num1 + num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    Console.WriteLine("Result: " + result);
+                    break;
+                case '/':
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine("Result: " + result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error:Cannot divide by zero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operator.");
+                    break;
+            }
+        }
+    }
+}
